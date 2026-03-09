fb7ea5a4aa03e67587aaef7795d4048800a702d5
Noah Ross <me@noah.dev>
2026-03-09 11:13:08 -0400

    Fix grammar in EC slot share error message

--- src/main/java/dev/noah/perplayerkit/commands/share/ShareECKitCommand.java
+++ src/main/java/dev/noah/perplayerkit/commands/share/ShareECKitCommand.java
@@ -23,6 +23,6 @@ import dev.noah.perplayerkit.KitShareManager;
 public class ShareECKitCommand extends AbstractShareSlotCommand {
 
     public ShareECKitCommand() {
-        super("Error, you must select a EC slot to share", (player, slot) -> KitShareManager.get().shareEC(player, slot));
+        super("Error, you must select an EC slot to share", (player, slot) -> KitShareManager.get().shareEC(player, slot));
     }
 }
